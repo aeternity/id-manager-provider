e4b358b749bf6f94adada56d59c7e8bc573b63e4
muxe <max@apeunit.com>
2017-11-10 16:59:30 +0100

    add IDs for registering callback handlers

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 import ZeroClientProvider from 'web3-provider-engine/zero'
 import Web3 from 'web3'
+const uuidv4 = require('uuid/v4')
 
 const METHOD_GET_ACCOUNTS = 'getAccounts'
-const METHOD_GET_ACCOUNTS_RETURN = 'getAccountsReturn'
+// const METHOD_GET_ACCOUNTS_RETURN = 'getAccountsReturn'
 const METHOD_SIGN_TRANSACTION = 'signTransaction'
-const METHOD_SIGN_TRANSACTION_RETURN = 'signTransactionReturn'
+// const METHOD_SIGN_TRANSACTION_RETURN = 'signTransactionReturn'
 
 class IdManagerProvider {
 	constructor (options) {
+		this.protocol = options.protocol ? options.protocol : 'http'
 		this.idManagerHost = options.idManagerHost ? options.idManagerHost : 'identity.aepps.com'
 		this.rpcUrl = options.rpcUrl ? options.rpcUrl : 'https://kovan.infura.io'
 		this.idManagerWindow = options.idManagerWindow ? options.idManagerWindow : parent
 		this.web3 = null
+		this.handlers = {}
 		this.init()
 	}
 
@@ -19,29 +22,14 @@ class IdManagerProvider {
 		let that = this
 		this.web3 = new Web3(new ZeroClientProvider({
 			getAccounts: function (done) {
-				let receiveHandler = that.getHandler(METHOD_GET_ACCOUNTS_RETURN, function (err, payload) {
-					// remove the handler after handled, yay
-					window.removeEventListener('message', receiveHandler)
-					if (err) {
-						return done(err)
-					}
+				that.postMessage(METHOD_GET_ACCOUNTS, null, function (payload) {
 					return done(null, payload)
 				})
-				window.addEventListener('message', receiveHandler, false)
-				that.postMessage(METHOD_GET_ACCOUNTS)
 			},
 			signTransaction: function (tx, done) {
-				let receiveHandler = that.getHandler(METHOD_SIGN_TRANSACTION_RETURN, function (err, payload) {
-					console.log('receiveHandler signTransaction', err, payload)
-					// remove the handler after handled, yay
-					window.removeEventListener('message', receiveHandler)
-					if (err) {
-						return done(err)
-					}
+				that.postMessage(METHOD_SIGN_TRANSACTION, tx, function (payload) {
 					return done(null, '0x' + payload)
 				})
-				window.addEventListener('message', receiveHandler, false)
-				that.postMessage(METHOD_SIGN_TRANSACTION, tx)
 			},
 			approveTransaction: function (tx, done) {
 				// auto approve
@@ -49,38 +37,39 @@ class IdManagerProvider {
 			},
 			rpcUrl: this.rpcUrl
 		}))
-	}
 
-	getHandler (desiredMethod, callback) {
-		return function (event) {
-			// console.log('receive message', event)
-			// TODO: origin check
-			// if (event.origin !== "http://identity.aepps.com") {
-			// 	return
-			// }
+		//register event listener
+		window.addEventListener('message', function (event) {
 			let data = event.data
-			if (!data.method) {
-				// console.log('no method in data')
-				// return callback(Error('no method in data'))
+			if (!data.uuid) {
+				// this message is not for us
 				return
 			}
-			let method = data.method
 			let payload = data.payload
-			if (method === desiredMethod) {
-				return callback(null, payload)
+			let handler = that.handlers[data.uuid]
+			if (handler) {
+				handler(payload)
+				delete that.handlers[data.uuid]
 			}
-		}
+		}, false)
 	}
 
-	postMessage (method, payload) {
+	postMessage (method, payload, callback) {
+		let uuid = uuidv4()
+		this.registerHandler(uuid, callback)
 		// TODO: define target origin
-		// let targetOrigin = '"http://identity.aepps.com"';
-		let targetOrigin = '*'
+		let targetOrigin = this.protocol + '://' + this.idManagerHost;
+		// let targetOrigin = '*'
 		let message = {
+			uuid: uuid,
 			method: method,
 			payload: payload
 		}
 		this.idManagerWindow.postMessage(message, targetOrigin)
+	}
+
+	registerHandler(uuid, handler) {
+		this.handlers[uuid] = handler
 	}
 
 	checkIdManager () {
